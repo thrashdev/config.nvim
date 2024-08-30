dd8ed74b46f85b03e23de778363b434bf4b71529
Danil Samsonov <thrashdev@protonmail.com>
2024-08-30 10:41:24 +0500

    added dockerls

--- init.lua
+++ init.lua
@@ -573,6 +573,7 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         pyright = {},
+        dockerls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
